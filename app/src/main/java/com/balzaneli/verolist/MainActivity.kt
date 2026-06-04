64321ed9de94770122b4843dde71271e9246ba62
Cauã Balzaneli <cauabalzaneli81@gmail.com>
2026-06-04 16:07:07 -0300

    add: dark mode

--- app/src/main/java/com/balzaneli/verolist/MainActivity.kt
+++ app/src/main/java/com/balzaneli/verolist/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.balzaneli.verolist.navigation.TodoNavHost
 import com.balzaneli.verolist.ui.theme.VeroListTheme
@@ -15,16 +18,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Box(
-                modifier = Modifier
-                    .safeDrawingPadding()
-            ){
-                VeroListTheme {
-                    TodoNavHost()
+            VeroListTheme(dynamicColor = false) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .safeDrawingPadding()
+                    ) {
+                        TodoNavHost()
+                    }
                 }
             }
-
         }
     }
 }
-
